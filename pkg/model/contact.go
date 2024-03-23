e5d4f481af13ddc2b84a4a9c0f77de2e38209ea7
Temirbaev Temirlan <temirlan213@bk.ru>
2024-03-23 12:36:08 +0500

    migrations

--- pkg/model/contact.go
+++ pkg/model/contact.go
@@ -4,6 +4,6 @@ type Contact struct {
 	Id          int      `json:"-" db:"id"`
 	Address     []string `json:"address" binding:"required"`
 	Phone       string   `json:"phone" binding:"required"`
-	VipPhone    string   `json:"vipPhone" binding:"required"`
+	VipPhone    string   `json:"vip_phone" binding:"required" db:"vip_phone"`
 	Coordinates []string `json:"coordinates" binding:"required"`
 }
