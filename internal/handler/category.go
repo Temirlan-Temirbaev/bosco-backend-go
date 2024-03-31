38f1ecd373b655a5a65d61c47ad3c85be1ca550f
Temirbaev Temirlan <temirlan213@bk.ru>
2024-03-31 16:52:31 +0500

    feature/categories

--- internal/handler/category.go
+++ internal/handler/category.go
@@ -1,23 +1,95 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"bosco-backend/internal/model"
+	"bosco-backend/internal/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func (h *Handler) createCategory(c *gin.Context) {
+	var input model.Category
+	if err := c.BindJSON(&input); err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	id, err := h.services.Category.Create(input)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getCategories(c *gin.Context) {
+	categories, err := h.services.Category.GetAll()
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"data": categories,
+	})
 
 }
 
 func (h *Handler) getCategoryById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	category, err := h.services.Category.GetById(id)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"data": category,
+	})
 }
 
 func (h *Handler) editCategory(c *gin.Context) {
+	var input model.Category
+	if err := c.BindJSON(&input); err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	err = h.services.Category.Update(id, input)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"success": true,
+	})
 }
 
 func (h *Handler) deleteCategory(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	err = h.services.Category.Delete(id)
+	if err != nil {
+		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"success": true,
+	})
 }
